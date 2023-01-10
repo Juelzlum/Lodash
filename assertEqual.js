4ad1b58b534c8f8ceb96c197ab1c9dc307d5c242
Juelz Lum <lumjulian@gmail.com>
2023-01-09 20:10:11 -0800

    Testing assertEqual

---
+++ assertEqual.js
@@ -0,0 +1,21 @@
+
+const sum = (a, b) => {
+  return a + b
+}
+
+console.assert(sum(1 , 2) === 3 )
+
+
+const assertEqual = function(actual, expected) {
+  if(actual === expected) {
+    console.log(`Assertion Passed ${actual} === ${expected}`)
+  }
+  else {
+    console.log(`Assertion Failed ${actual} !== ${expected}`)
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(1, 2)
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
