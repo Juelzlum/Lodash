57406c35273f2562c3b1a6df3604490619ee2880
vagrant <Juelz Lum>
2023-01-17 00:20:01 +0000

    refactoring

--- head.js
+++ head.js
@@ -1,8 +1,7 @@
-const assertEqual = require('./Test/ assertEqualTest')
+const assertEqual = require('./assertEqual')
+
 const head = (array)=> {
   return array[0]
 }
 
-
-assertEqual(head([5,6,7]), 6);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head
