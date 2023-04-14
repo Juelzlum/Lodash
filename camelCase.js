afe0e7b2bf2df211e536c907e687052f9be90ae7
Julian Lum <lumjulian@gmail.com>
2023-04-14 14:03:23 -0700

    completed

--- camelCase.js
+++ camelCase.js
@@ -8,10 +8,30 @@ console.log(camelCase("loopy lighthouse"));
 console.log(camelCase("supercalifragalisticexpialidocious"));
 
 
+// const sumLargestNumbers = function(data) {
+//   let bigNum = 0
+//   let numTwo = 0
+//   for(let i = 0;  i < data.length ; i++) {
+//     if (data[i]> bigNum && data[i] != numTwo) {
+//       bigNum = data[i]
+//     for(let j = 0; j< data.length; j++) {
+//       if(data[j]> numTwo && data[j] != bigNum) 
+//         numTwo = data[j]
+//     }
+//     }
+//     }
+//     return bigNum + numTwo
+//   }
+
 const sum = (data) => {
 const max = Math.max(...data)
-console.log(max)
-
+for(let i = 0 ; i < data.length; i ++) {
+  if(data[i] < max) {
+    numTwo = Math.max(data[i])
+    
+  }
+}
+return max + numTwo
 }
 
-console.log(sum([1,2,3]))
+console.log(sum([11,2,3,4,5,6]))
