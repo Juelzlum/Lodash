a0cf000d1c62cf504a11304113441d0a610ac9e6
Julian Lum <lumjulian@gmail.com>
2023-04-13 22:27:05 -0700

    add password.js

--- concepts.js
+++ concepts.js
@@ -1,15 +1,19 @@
 const joinList = (list) => {
-  let result = ''
-//loop the arrays
-  for(let i = 0; i < list.length ; i++ ) {
-    if(typeof(list[i]) === 'string') {
-  //print the array index out 
-      result += list[i] }
-    if(i < list.length - 1) {
-      result += ", " } }
-  return result }
+  let result = '';
+  //loop the arrays
+  for (let i = 0; i < list.length; i++) {
+    if (typeof (list[i]) === 'string') {
+      //print the array index out 
+      result += list[i];
+    }
+    if (i < list.length - 1) {
+      result += ", ";
+    }
+  }
+  return result;
+};
 
-  
-  const conceptList = ["gists", "types", "operators", "iteration", "problem solving"];
-  const concepts = joinList(conceptList);
-  console.log(`Today I learned about ${concepts}.`)
+
+const conceptList = ["gists", "types", "operators", "iteration", "problem solving"];
+const concepts = joinList(conceptList);
+console.log(`Today I learned about ${concepts}.`);
