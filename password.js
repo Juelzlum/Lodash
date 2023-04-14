c07c510c42d5be8a6b9038956c7bd3eb0bd3d5f0
Julian Lum <lumjulian@gmail.com>
2023-04-13 23:51:03 -0700

    add c style loop

--- password.js
+++ password.js
@@ -2,7 +2,9 @@ const args = process.argv
 const values = args.slice(2)
 
 const obfuscate  = (password) => {
-  
+ for(let i = 0 ; i < password.length; i ++) {
+  console.log(password[i])
+ }
 } 
 
 // Every "a" in the string should be replaced with a "4".
