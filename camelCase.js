0d5ab3fcc04967cde88bf3927fc8c78d33ab5f39
Julian Lum <lumjulian@gmail.com>
2023-04-14 13:46:50 -0700

    playing with spread and addition

--- camelCase.js
+++ camelCase.js
@@ -1,7 +1,17 @@
 const camelCase = function(input) {
- input.split(' ')
+ const str = input.split('')
+ console.log(str)
 };
 
 console.log(camelCase("this is a string"));
 console.log(camelCase("loopy lighthouse"));
 console.log(camelCase("supercalifragalisticexpialidocious"));
+
+
+const sum = (data) => {
+const max = Math.max(...data)
+console.log(max)
+
+}
+
+console.log(sum([1,2,3]))
