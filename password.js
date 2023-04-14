1a17ee27e89518757c394927f4e6c823af0cda09
Julian Lum <lumjulian@gmail.com>
2023-04-13 23:42:32 -0700

    testing password.js

---
+++ password.js
@@ -0,0 +1,12 @@
+const args = process.argv
+const values = args.slice(2)
+
+const obfuscate  = (password) => {
+  
+} 
+
+// Every "a" in the string should be replaced with a "4".
+// Every "e" in the string should be replaced with a "3".
+// Every "o" (oh) in the string should be replaced with a "0" (zero).
+// Every "l" (el) in the string should be replaced with a "1" (one).
+obfuscate(values)
